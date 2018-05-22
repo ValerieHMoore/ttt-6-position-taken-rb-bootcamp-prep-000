5c91ea6fde4914fafafff7bc42a459c5d2397f49
Valerie Moore <periwinkle17@outlook.com>
2018-05-22 21:02:12 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,12 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
-  
+  if board[index_number] == " "
+    false
+    elsif board[index_number] == ""
+    false
+    elsif board[index_number] == "X"
+    true
+    elsif board[index_number] == "O"
+    true
+  else nil
 end
